c80c76b3ef596c793a3035d3c783a80722efdf12
Abdulrahman <55775237+Abdo-461@users.noreply.github.com>
2021-05-05 08:56:26 +1000

    Add amazon access code for dynamodb and s3 bucket

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , request, flash , redirect , url_for , json , session
+import boto3
+import re
+from boto3.dynamodb.conditions import Key
+
+#create dynamo object with access and secret keys
+dynamodb = boto3.resource('dynamodb',aws_access_key_id='',
+                                     aws_secret_access_key='',
+                                     region_name='us-east-1')
+
+#s3 object to define s3 bucket
+s3 = boto3.resource('s3', aws_access_key_id='',
+                          aws_secret_access_key='',
+                          region_name='us-east-1')
+bucket = s3.Bucket('')
 
 application = Flask(__name__)
 
