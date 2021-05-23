8e9b2f2598a2d55cdc125944cac257584c9b9021
Andrew <s3547477@student.rmit.edu.au>
2021-05-23 17:27:54 +1000

    Added both oauth logins

--- app.py
+++ app.py
@@ -1,52 +1,134 @@
-from flask import Flask, render_template, request, flash , redirect , url_for , json , session
+from flask import Flask, render_template, request, flash, redirect, url_for, json, session
+from flask_dance.contrib.github import make_github_blueprint, github
+from flask_dance.contrib.google import make_google_blueprint, google
 from decimal import Decimal
 import boto3
 import re
+import os
 from boto3.dynamodb.conditions import Key
 
+github_blueprint = make_github_blueprint(
+    scope=["email"],
+    client_id=os.getenv("GITHUB_ID"),
+    client_secret=os.getenv("GITHUB_SECRET"),
+)
 
-#create dynamo object with access and secret keys
-dynamodb = boto3.resource('dynamodb',aws_access_key_id='',
-                                     aws_secret_access_key='',
-                                     region_name='us-east-1')
+google_blueprint = make_google_blueprint(
+    client_id=os.getenv("GOOGLE_ID"),
+    client_secret=os.getenv("GOOGLE_SECRET"),
+    scope=["profile", "email"]
+)
 
-#s3 object to define s3 bucket
-s3 = boto3.resource('s3', aws_access_key_id='',
-                          aws_secret_access_key='',
+# create dynamo object with access and secret keys
+dynamodb = boto3.resource('dynamodb', aws_access_key_id=os.getenv("AWS_ID"),
+                          aws_secret_access_key=os.getenv("AWS_KEY"),
                           region_name='us-east-1')
+
+# s3 object to define s3 bucket
+s3 = boto3.resource('s3', aws_access_key_id='',
+                    aws_secret_access_key='',
+                    region_name='us-east-1')
 bucket = s3.Bucket('')
 
 application = Flask(__name__)
 
 
-#Login function --START--
+# Login function --START--
 @application.route("/", methods=['POST', 'GET'])
 def login():
-    if request.method=='POST':
-        #get user information from form
+    if request.method == 'POST':
+        # get user information from form
         email = request.form['email']
         password = request.form['password']
-        #get table
+        # get table
         table = dynamodb.Table('users')
-        #query table and comapre email
+        # query table and comapre email
         response = table.query(
-                KeyConditionExpression=Key('user_email').eq(email)
+            KeyConditionExpression=Key('user_email').eq(email)
         )
-        #put results in items
+        # put results in items
         items = response['Items']
         for item in items:
             session['user_name'] = item['user_name']
             session['user_email'] = item['user_email']
-            #comapre password
+            # comapre password
             if password == item['password']:
                 return redirect(url_for('dashboard'))
 
         flash("Email or Password Invalid")
     return render_template('login.html')
 
-#Login function --END--
+# Login function --END--
 
-#Register function --START--
+# Github Login function --START--
+
+
+@application.route("/github")
+def githubLogin():
+    if not github.authorized:
+        return redirect(url_for("github.login"))
+    res = github.get("/user")
+    if res.ok:
+        oauth = github_blueprint.session.access_token
+        account_info = res.json()
+        session['user_name'] = account_info["name"]
+        session['user_email'] = account_info["email"]
+        # check if user is in the DB otherwise add email, password, oauth token
+        table = dynamodb.Table('oauthusers')
+        # Query table to see if email exists
+        response = table.query(
+            KeyConditionExpression=Key('user_email').eq(session['user_email'])
+        )
+        items = response['Items']
+        # If email doesn't exist add information to database table
+        if not items:
+            table.put_item(
+                Item={
+                    'user_email': session['user_email'],
+                    'user_name': session['user_name'],
+                    'oauth_token': oauth
+                }
+            )
+    # validate oauth token
+    if oauth == response['Items'][0]['oauth_token'] :
+        return redirect(url_for('dashboard'))
+# Github Login function --END--
+
+# Google Login function --START--
+@application.route("/google")
+def googleLogin():
+    if not google.authorized:
+        return redirect(url_for("google.login"))
+    res = google.get("/oauth2/v1/userinfo")
+    if res.ok:
+        oauth = google_blueprint.session.access_token
+        account_info = res.json()
+        session['user_name'] = account_info["name"]
+        session['user_email'] = account_info["email"]
+        # check if user is in the DB otherwise add email, password, oauth token
+        table = dynamodb.Table('oauthusers')
+        # Query table to see if email exists
+        response = table.query(
+            KeyConditionExpression=Key('user_email').eq(session['user_email'])
+        )
+        items = response['Items']
+        # If email doesn't exist add information to database table
+        if not items:
+            table.put_item(
+                Item={
+                    'user_email': session['user_email'],
+                    'user_name': session['user_name'],
+                    'oauth_token': oauth
+                }
+            )
+    # validate oauth token
+    if oauth == response['Items'][0]['oauth_token']:
+        return redirect(url_for('dashboard'))
+# Google Login function --END--
+
+# Register function --START--
+
+
 @application.route("/register", methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
@@ -68,7 +150,7 @@ def register():
                 Item={
                     'user_email': email,
                     'user_name': name,
-                    'income':income,
+                    'income': income,
                     'password': password
                 }
             )
@@ -79,16 +161,18 @@ def register():
         flash("Email already Exists")
     return render_template('register.html')
 
-#Register function --END--
+# Register function --END--
 
-#logout function --START--
+# logout function --START--
+
+
 @application.route('/logout')
 def logout():
-    session.pop('email', None)
-    session.pop('name', None)
+    session.pop('user_email', None)
+    session.pop('user_name', None)
     return redirect('/')
 
-#logout function --END--
+# logout function --END--
 
 
 @application.route("/dashboard")
@@ -96,37 +180,38 @@ def dashboard():
     return render_template('dashboard.html', username=session['user_name'])
 
 
-@application.route("/credit" , methods=["GET"])
+@application.route("/credit", methods=["GET"])
 def credit():
     return render_template('creditcheck.html', username=session['user_name'])
 
+
 @application.route("/userprofile", methods=["POST", "GET"])
 def userprofile():
-    if request.method=='POST':
-        #get user information from form
+    if request.method == 'POST':
+        # get user information from form
         oldPassword = request.form['old-password']
         newPassword = request.form['new-password']
-        #get table
+        # get table
         table = dynamodb.Table('users')
-        #query table and comapre email
+        # query table and comapre email
         response = table.query(
-                KeyConditionExpression=Key('user_email').eq(session['user_email'])
+            KeyConditionExpression=Key('user_email').eq(session['user_email'])
         )
-        #put results in items
+        # put results in items
         items = response['Items']
         for item in items:
-            #compare password
+            # compare password
             if oldPassword == item['password']:
                 response = table.update_item(
                     Key={
                         'user_email': session['user_email']
-                        },
-                        UpdateExpression="set password = :p",
-                        ExpressionAttributeValues={
-                            ':p': newPassword,
-                            },
-                            ReturnValues="UPDATED_NEW"
-                            )
+                    },
+                    UpdateExpression="set password = :p",
+                    ExpressionAttributeValues={
+                        ':p': newPassword,
+                    },
+                    ReturnValues="UPDATED_NEW"
+                )
                 flash('Your Password has successfully been changed!')
                 return redirect(url_for('userprofile', username=session['user_name']))
 
@@ -134,176 +219,184 @@ def userprofile():
 
     return render_template('userprofile.html', username=session['user_name'])
 
+
 @application.route('/loans')
 def loans():
     return render_template('loans.html', username=session['user_name'])
 
 
-#Credit check function --START--
+# Credit check function --START--
 
-#paid on time list - Loan
-loanPaidOnTime=list()
-#paid late list
-loanPaidLate=list()
+# paid on time list - Loan
+loanPaidOnTime = list()
+# paid late list
+loanPaidLate = list()
 
-#paid on time list - Credit Card
-cardPaidOnTime=list()
-#paid late list
-cardPaidLate=list()
+# paid on time list - Credit Card
+cardPaidOnTime = list()
+# paid late list
+cardPaidLate = list()
 
-#paid on time list - Bills
-billPaidOnTime=list()
-#paid late list
-billPaidLate=list()
+# paid on time list - Bills
+billPaidOnTime = list()
+# paid late list
+billPaidLate = list()
+
+
 @application.route("/creditCheck")
 def creditCheck():
 
-    #loan repayment
-    #refernce table
+    # loan repayment
+    # refernce table
     loanTable = dynamodb.Table('LoanHistory')
-    #query table
+    # query table
     loanResponse = loanTable.query(
         KeyConditionExpression=Key('user_email').eq(session['user_email'])
     )
-    #put response in items variable
+    # put response in items variable
     loanItems = loanResponse['Items']
-    #iterate through the items and compare paid off time with terms to determine loan payments health
+    # iterate through the items and compare paid off time with terms to determine loan payments health
     for loans in loanItems:
         if loans['paid_off_term'] <= loans['terms']:
             loanPaidOnTime.append(loans['loan_status'])
         elif loans['paid_off_term'] > loans['terms']:
             loanPaidLate.append(loans['loan_status'])
-    #compare amount of on time and late payments and assign score
+    # compare amount of on time and late payments and assign score
     if len(loanPaidOnTime) < len(loanPaidLate):
-        loanScore=150
+        loanScore = 150
     elif len(loanPaidOnTime) > len(loanPaidLate):
-        loanScore=300
+        loanScore = 300
     elif len(loanPaidOnTime) == len(loanPaidLate):
-        loanScore=260
+        loanScore = 260
 
-    #credit card repayment
-    #refernce table
+    # credit card repayment
+    # refernce table
     creditCardTable = dynamodb.Table('CreditCardPayments')
-    #query table
+    # query table
     creditCardResponse = creditCardTable.query(
         KeyConditionExpression=Key('user_email').eq(session['user_email'])
     )
-    #put items in items variable
+    # put items in items variable
     cardItems = creditCardResponse['Items']
-    #iterate through the items and compare duration time with payment duration to determine credit card health
+    # iterate through the items and compare duration time with payment duration to determine credit card health
     for credit in cardItems:
         if credit['payment_duration'] <= credit['duration']:
             cardPaidOnTime.append(credit['status'])
         elif credit['payment_duration'] > credit['duration']:
             cardPaidLate.append(credit['status'])
-    #compare amount of on time and late payments and assign score
+    # compare amount of on time and late payments and assign score
     if len(cardPaidOnTime) < len(cardPaidLate):
-        cardScore=150
+        cardScore = 150
     elif len(cardPaidOnTime) > len(cardPaidLate):
-        cardScore=300
+        cardScore = 300
     elif len(cardPaidOnTime) == len(cardPaidLate):
-        cardScore=260
+        cardScore = 260
 
-    #bills payment
-    #reference table
+    # bills payment
+    # reference table
     billsPaymentTable = dynamodb.Table('BillsPayment')
-    #query table
+    # query table
     billsResponse = billsPaymentTable.query(
         KeyConditionExpression=Key('user_email').eq(session['user_email'])
     )
-    #put items in items variable
+    # put items in items variable
     billItems = billsResponse['Items']
-    #iterate through the items and compare duration time with payment duration to determine credit card health
+    # iterate through the items and compare duration time with payment duration to determine credit card health
     for bills in billItems:
         if bills['pay_on_time'] == 'YES':
             billPaidOnTime.append(bills['pay_on_time'])
         billPaidLate.append(bills['pay_on_time'])
-    #compare amount of on time and late payments and assign score
+    # compare amount of on time and late payments and assign score
     if len(billPaidOnTime) < len(billPaidLate):
-        billScore=150
+        billScore = 150
     elif len(billPaidOnTime) > len(billPaidLate):
-        billScore=300
+        billScore = 300
     elif len(billPaidOnTime) == len(billPaidLate):
-        billScore=260
+        billScore = 260
 
-    #calculate total credit score
+    # calculate total credit score
     creditScore = loanScore + cardScore + billScore
-    #show analysis of credit score
+    # show analysis of credit score
     if 400 <= creditScore <= 500:
-        message='Your Credit Score is very low.'
+        message = 'Your Credit Score is very low.'
     elif 550 <= creditScore <= 700:
-        message='You have a healthy credit. Could be better.'
+        message = 'You have a healthy credit. Could be better.'
     elif creditScore > 700:
-        message='You have excellent credit!'
+        message = 'You have excellent credit!'
 
-    return render_template('creditcheck.html' , loanScore = loanScore , cardScore = cardScore,billScore = billScore, creditScore = creditScore, message = message, username=session['user_name'])
+    return render_template('creditcheck.html', loanScore=loanScore, cardScore=cardScore, billScore=billScore, creditScore=creditScore, message=message, username=session['user_name'])
 
-#Credit Check function --END--
+# Credit Check function --END--
 
-#Save Credit Score function --START--
+# Save Credit Score function --START--
 
-#save credit score to database
-@application.route('/saveCredit' , methods=["POST"])
+# save credit score to database
+
+
+@application.route('/saveCredit', methods=["POST"])
 def saveCredit():
- if request.method=='POST':
-    #fetch credit score
-    creditScore = request.form['creditscore']
-    #referene table
-    creditScoreTable = dynamodb.Table('CreditScores')
-    #validate if input is empty
-    if creditScore is None:
-        flash('You need to check your credit first')
-        return redirect(url_for('credit'))
+    if request.method == 'POST':
+        # fetch credit score
+        creditScore = request.form['creditscore']
+        # referene table
+        creditScoreTable = dynamodb.Table('CreditScores')
+        # validate if input is empty
+        if creditScore is None:
+            flash('You need to check your credit first')
+            return redirect(url_for('credit'))
 
-    creditScoreTable.put_item(
-        Item={
-            'user_email':session['user_email'],
-            'credit_score':creditScore
-        }
-    )
- flash("Score Saved!")
- return redirect(url_for('creditCheck'))
+        creditScoreTable.put_item(
+            Item={
+                'user_email': session['user_email'],
+                'credit_score': creditScore
+            }
+        )
+    flash("Score Saved!")
+    return redirect(url_for('creditCheck'))
 
-#Save credit score function --end--
+# Save credit score function --end--
 
-#check elligble loans function --START--
+# check elligble loans function --START--
+
 
 @application.route('/eligibleLoans')
 def eligibleLoans():
 
-    #refernece credit table
+    # refernece credit table
     creditTable = dynamodb.Table('CreditScores')
-    #query credit table to fetch user's credit score
+    # query credit table to fetch user's credit score
     creditScoreResponse = creditTable.query(
         KeyConditionExpression=Key('user_email').eq(session['user_email'])
     )
-    #put response in item variable
+    # put response in item variable
     creditScoresItems = creditScoreResponse['Items']
 
-    #create variable for credit score
+    # create variable for credit score
     for creditScoreLoans in creditScoresItems:
-      creditScore = creditScoreLoans['credit_score']
+        creditScore = creditScoreLoans['credit_score']
 
-    #refernce loan table
+    # refernce loan table
     eligibleLoanTable = dynamodb.Table('EligibleLoans')
-    #query table to fetch loans depending on user's credit score
+    # query table to fetch loans depending on user's credit score
     eligibleLoanResponse = eligibleLoanTable.query(
-        IndexName = 'credit_score_Index',
+        IndexName='credit_score_Index',
         KeyConditionExpression=Key('min_cs').eq(creditScore)
     )
-    #put items in Item variable
+    # put items in Item variable
     loanItems = eligibleLoanResponse["Items"]
     if not loanItems:
         flash("Sorry, you are not eligible for any loans")
 
-    return render_template('loans.html', elgibleLoan = loanItems)
+    return render_template('loans.html', elgibleLoan=loanItems)
 
 
-#check elligble loans function --END--
+# check elligble loans function --END--
 
 
 application.secret_key = 'super secret key'
 application.config['SESSION_TYPE'] = 'filesystem'
+application.register_blueprint(github_blueprint, url_prefix="/login")
+application.register_blueprint(google_blueprint, url_prefix="/login")
 
 if __name__ == '__main__':
-    application.run(port=8000,debug=True)
+    application.run(port=8000, debug=True)
